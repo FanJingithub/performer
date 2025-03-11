3187d28be78c8c17863624935cfbc43fa9f20ca2
fanjin <fanjin@inftech.ai>
2025-03-11 10:03:14 +0800

    fix(fab) fix fabfile cd

--- fabfile.py
+++ fabfile.py
@@ -18,6 +18,7 @@ version = app_info["version"]
 app_dir = app_info["app_dir"]
 app_dir_www = app_dir + "/www"
 
+_RELEASE_NAME = 'crf'
 _REMOTE_BASE_DIR = '/home/ubuntu/web'
 _TAR_FILE = "webfiles.tar.gz"
 
@@ -37,7 +38,7 @@ def upload(c):
     subprocess.run('rm -f dist/webfiles.tar.gz', shell=True, check=True)
     
     # 创建新的压缩文件
-    cmd = ['tar', '--dereference', '-czvf', f'dist/{_TAR_FILE}', 'crf']
+    cmd = ['tar', '--dereference', '-czvf', f'dist/{_TAR_FILE}', _RELEASE_NAME]
     subprocess.run(cmd, check=True)
     
     # 删除远程服务器上的旧文件
@@ -46,9 +47,10 @@ def upload(c):
     # 上传新的压缩文件
     conn.put(f'dist/{_TAR_FILE}', _REMOTE_BASE_DIR)
     
+    # 清理解压缩目标目录
+    conn.run(f"rm -rf {_REMOTE_BASE_DIR}/{_RELEASE_NAME}")
     # 解压缩文件
-    with conn.cd(_REMOTE_BASE_DIR):
-        conn.sudo(f'tar -xzvf {_TAR_FILE}')
+    conn.run(f'cd {_REMOTE_BASE_DIR} && tar -xzvf {_TAR_FILE}')
 
 @task
 def download(c):
@@ -58,11 +60,10 @@ def download(c):
 @task
 def deploy(c):
     # 执行远程清理脚本
-    conn.run(f"bash {_REMOTE_BASE_DIR}/crf/shell/clear.sh")
+    conn.run(f"bash {_REMOTE_BASE_DIR}/{_RELEASE_NAME}/shell/clear.sh")
+
+    if version == "0":
+        conn.run(f"bash {app_dir_www}/sql.sh")
+        conn.run(f"bash {app_dir_www}/nginx.sh")
     
-    # 进入应用目录并执行脚本
-    with conn.cd(f"{app_dir_www}/shell"):
-        if version == "0":
-            conn.run("bash sql.sh")
-            conn.run("bash nginx.sh")
-        conn.run("bash supervisor.sh")
+    conn.run(f"bash {app_dir_www}/supervisor.sh")
